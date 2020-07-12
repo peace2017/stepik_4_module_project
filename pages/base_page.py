759b610fb198a21758e2a9cfa793146c845bc073
peace_oh <peace_oh@inbox.ru>
2020-07-12 16:37:54 +0300

    add test main page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
 class BasePage():
-	def __init__(self, browser, url):
-    self.browser = browser
-    self.url = url
-    
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,5 @@
+from .pages.main_page import MainPage
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
